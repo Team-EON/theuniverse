15d7d4b675640d38eeb84c040d6114e1704f44db
robinmollah <robinsajin@gmail.com>
2019-10-06 04:31:32 +0600

    Switching from MainMenu | Auto initial force generation

--- Assets/Planet.cs
+++ Assets/Planet.cs
@@ -5,14 +5,28 @@ using UnityEngine;
 public class Planet : UniverseEntity
 {
     private Rigidbody rb;
-    public Vector3 initialForce;
-    public Rigidbody anotherPlanet;
-    private float GravitationalConstant = 10f;
+    private Vector3 initialForce;
+    private Rigidbody anotherPlanet;
+    public float distanceFromStar = 7;
     private float massMultiple;
     private float mass1, mass2;
 
     private void Start()
     {
+        // distanceFromStar *= 1000; // Converting to our unit [1000km = 1ou]
+        base.Start();
+
+        // We are just plotting in z axis now for test purpose. Do the following to input exact position from the star.
+        // 1. change data type of distanceFromStar to Vector3
+        // 2. "new Vector3(0, 0, distanceFromStar)" > "distanceFromStar"
+        // Now you will be able to input the vector position of the planet.
+        // Note: now the actual distance from star is the distanceFromStar.magnitude
+        this.transform.position = new Vector3(0, 0, distanceFromStar);
+
+        // TODO Get a formular to get such initial factor that the elliptical path's eccentricity get close to 1.
+        // initialForce depends on mass and distance from star
+        initialForce = new Vector3(ifDisRate * distanceFromStar, 0, 0);
+        anotherPlanet = GameObject.FindGameObjectWithTag("Sun").GetComponent<Rigidbody>();
         this.rb = GetComponent<Rigidbody>();
         massMultiple = GravitationalConstant * this.rb.mass * this.anotherPlanet.mass;
         rb.AddForce(initialForce);
@@ -20,9 +34,9 @@ public class Planet : UniverseEntity
 
     void FixedUpdate()
     {
-        Vector3 direction = anotherPlanet.transform.position - rb.transform.position;
-        float gForce = massMultiple / Mathf.Pow(direction.magnitude, 2);
-        initialForce = initialForce + (direction.normalized * gForce);
-        rb.AddForce(direction.normalized * gForce);
+        Vector3 distance = anotherPlanet.transform.position - rb.transform.position;
+        float gForce = massMultiple / Mathf.Pow(distance.magnitude, 2);
+        Debug.Log("Direction: " + distance.magnitude + ", " + gForce);
+        rb.AddForce(distance.normalized * gForce);
     }
 }
--- Assets/UniverseEntity.cs
+++ Assets/UniverseEntity.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class UniverseEntity : MonoBehaviour
 {
+    [System.NonSerialized]
+    public float GravitationalConstant = 6f;
+    public float radius = 1f;
+    [System.NonSerialized]
+    // Ratio of initForce and distance. [Remove it after deriving initForce formula.]
+    public float ifDisRate = 6.5857f;
 
-    public float diameter = 1f;
-
-    private void Start()
+    protected void Start()
     {
-        this.transform.localScale *= diameter;
+        this.transform.localScale = new Vector3(radius, radius, radius);
     }
 }
