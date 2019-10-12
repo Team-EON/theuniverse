3d44bb1cf45e605bafce1e598442895c17ba20ef
Somoy Subandhu <Somoy97@gmail.com>
2019-10-12 15:15:48 +0600

    Added Cutomize Demo scripts, vuforia engine data

---
+++ Assets/Customize_menu/Scripts/Menu_slider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu_slider : MonoBehaviour
+{
+    public float radius = 250f;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = new Vector3(radius, radius, radius);
+    }
+
+    public void AdjustRadius(float radiusx)
+    {
+        radius = radiusx;
+    }
+}
---
+++ Assets/Customize_menu/Scripts/Rotate_speed.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate_speed : MonoBehaviour
+{
+    public float speed = 1f;
+    void Update()
+    {
+        transform.Rotate(0,speed * Time.deltaTime,0);
+    }
+
+    public void AdjustSpeed(float newSpeed)
+    {
+        speed = newSpeed;
+    }
+}
