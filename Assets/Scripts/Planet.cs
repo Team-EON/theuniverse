00df2f2764a0a2c60c8cda5702a054a2dabf6deb
robinmollah <robinsajin@gmail.com>
2019-10-18 03:47:58 +0600

    Camera following planet and zoom in out

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : UniverseEntity
+{
+    private Rigidbody rb;
+    public Vector3 initialForce;
+    public float distanceFromStar = 7;
+    private Rigidbody anotherPlanet;
+    private float mass1, mass2;
+    private float massMultiple;
+
+    private void Start()
+    {
+        this.transform.position = new Vector3(0, 0, distanceFromStar);
+        anotherPlanet = GameObject.FindGameObjectWithTag("Sun").GetComponent<Rigidbody>();
+        this.rb = GetComponent<Rigidbody>();
+        massMultiple = (GravitationalConstant * this.rb.mass * this.anotherPlanet.mass);
+        rb.AddForce(initialForce);
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 distance = anotherPlanet.transform.position - rb.transform.position;
+        float gForce = massMultiple / Mathf.Pow(distance.magnitude, 2);
+        rb.AddForce(distance.normalized * gForce);
+
+        // Debug.Log("Distance: " + distance.magnitude + ", Force: " + gForce);
+    }
+}
