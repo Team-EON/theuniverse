a67926958588990d676e30fd0620d9fdd48098f5
robinmollah <robinsajin@gmail.com>
2019-10-18 17:19:51 +0600

    Follow a planet

---
+++ Assets/Scripts/PlanetReader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetReader : MonoBehaviour
+{
+    public TextAsset csv;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CSVReader.DebugOutputGrid(CSVReader.SplitCsvGrid(csv.text));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
